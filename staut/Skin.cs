fcb49cdcc5df946ee78fca108692d56fe83c2237
Matheus-Boff <153229200+Matheus-Boff@users.noreply.github.com>
2025-07-11 14:22:24 -0300

    Feature/item (#6)
    * Item/Card/Skin construtores e propriedades
    
    * Name alteration in item attributes, price setting logic

--- staut/Skin.cs
+++ staut/Skin.cs
@@ -1,8 +1,17 @@
 using System;
 
 namespace Staut {
-    public class Skin {
-        Item baseItem;
-        String texture;
+    public class Skin : Item {
+        private Item _baseItem;
+        private string _texture;
+
+        public Item BaseItem { get; }
+        public string texture { get; set; }
+
+        public Skin(Item baseItem, string texture, int id, string name, double price, Game game, string description, Rarity rarity) : Item(int id, string name, double price, Game game, string description, Rarity rarity)
+        {
+            _baseItem = baseItem;
+            _texture = texture;
+        }
     }
 }

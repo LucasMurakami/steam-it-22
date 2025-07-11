1bed62cef5b120bfcfdda93a0604a36751087cf5
Henrique-Schultz <valente.schultz@gmail.com>
2025-07-11 15:04:07 -0300

    Feature/cart (#9)
    * Cart feature with sfx
    
    * modificações em cart
    
    * adição do maldito obj
    
    * tentativa de resolver a obj
    
    * nova gitignore

--- staut/Cart.cs
+++ staut/Cart.cs
@@ -6,14 +6,19 @@ using System.IO;
 
 namespace Staut {
     public class Cart {
-        List<Game>? games;
-        List<Item>? items;
-        double totalPrice;
-        int gems;
+        private List<Game>? games;
+        private List<Item>? items;
+        private double totalPrice;
+        private int gems;
+        private User user;
 
         static string moneySound;
         SoundPlayer player;
 
+        static void Main(string[] args) {
+            Cart cart = new Cart();
+            cart.purchase();
+        }
         public Cart() {
             moneySound = "sounds/money.wav";
             player = new SoundPlayer(moneySound);
@@ -22,37 +27,44 @@ namespace Staut {
 
 
         public bool purchase() {
-            Console.WriteLine("Aperte qualquer tecla para comprar");
+            Console.Clear();
+            Console.WriteLine("Seu saldo: 0.0");
+            Console.WriteLine("Total da compra: R$ " + totalPrice);
+            Console.WriteLine("Aperte \"Enter\" para confirmar a compra");
+
             Console.ReadKey();
+            Console.Clear();
+            Console.WriteLine("Compra realizada com sucesso!");
             player.PlaySync();
+
 
             return true;
         }
 
-        // public void listGames() {
-        //     StringBuilder builder = new StringBuilder();
-        //     foreach (Game game in games) {
-        //         builder.Append(game.getName() + " | " + game.getPrice() + "\n");
-        //     }
-        // }
+        public void listGames() {
+            StringBuilder builder = new StringBuilder();
+            foreach (Game game in games) {
+                builder.Append(game.Name + " | " + game.Price + "\n");
+            }
+        }
 
-        // public void listItems() {
-        //     StringBuilder builder = new StringBuilder();
-        //     foreach (Item item in items) {
-        //         builder.Append(item.getName() + " | " + item.getPrice() + "\n");
-        //     }
-        // }
+        public void listItems() {
+            StringBuilder builder = new StringBuilder();
+            // foreach (Item item in items) {
+            //     builder.Append(item.Name + " | " + item.Price + "\n");
+            // }
+        }
         
-        // public void listCart() {
-        //     StringBuilder builder = new StringBuilder();
-        //     builder.Append("== GAMES ==\n");
-        //     foreach (Game game in games) {
-        //         builder.Append(game.getName() + " | " + game.getPrice() + "\n");
-        //     }
-        //     builder.Append("== GAMES ==\n");
-        //     foreach (Item item in items) {
-        //         builder.Append(item.getName() + " | " + item.getPrice() + "\n");
-        //     }
-        // }
+        public void listCart() {
+            StringBuilder builder = new StringBuilder();
+            builder.Append("== GAMES ==\n");
+            foreach (Game game in games) {
+                builder.Append(game.Name + " | " + game.Price + "\n");
+            }
+            // builder.Append("== GAMES ==\n");
+            // foreach (Item item in items) {
+            //     builder.Append(item.Name + " | " + item.Price + "\n");
+            // }
+        }
     }
 }
